a8c6c7aae32b30c0e44efc909f39dc30659aa0fc
PivotA <ankief@gmail.com>
2022-05-12 14:08:13 +0200

    Fixed mistakes in calculating errors

--- Numerical Math/matlab/assign3/FourierAppro.m
+++ Numerical Math/matlab/assign3/FourierAppro.m
@@ -5,11 +5,11 @@ A(1) = double(1/pi*int(F*cos(0*sym('x')),[-pi,pi]));
 f = A(1)/2;
 e = 0;
 for i = 1:k
-   A(i+1) = 1/pi*int(F*cos(i*sym('x')),[-pi,pi]);
-   f = f + A(i+1)*cos(i*sym('x'));
-   B(i) = 1/pi*int(F*sin(i*sym('x')),[-pi,pi]);
-   f = f + B(i)*sin(i*sym('x'));
-   e = e + (A(i+1)^2+B(i)^2);
+    A(i+1) = 1/pi*int(F*cos(i*sym('x')),[-pi,pi]); 
+    % if using simpson:  A(i+1) = 1/pi*simpson(-pi,pi,10,F*cos(i*sym('x'))); 
+    f = f + A(i+1)*cos(i*sym('x'));
+    B(i) = 1/pi*int(F*sin(i*sym('x')),[-pi,pi]);
+    f = f + B(i)*sin(i*sym('x'));
 end
-e = e*pi;
+e = double(int((F-f)^2,[-pi,pi]));
 end
--- Numerical Math/matlab/assign3/answer3.m
+++ Numerical Math/matlab/assign3/answer3.m
@@ -2,13 +2,19 @@ F = cos(2*sym('x'))/(4+3*sin(sym('x')));
 
 figure(1)
 hold on
+format long
 
+Fun = @(x) subs(F,x);
+fplot(Fun,[-pi,pi])
+
+% f is the trigonometric polynomial
+% A and B are array of coefficients
+% e is error
 for i=1:6
     [f,A,B,e] = FourierAppro(F,i)
     if i<4
         fun = @(x) subs(f,x);
-        Fun = @(x) subs(F,x);
-        fplot(Fun,[0,50*pi])
-        fplot(fun,[0,50*pi])
+        fplot(fun,[-pi,pi])
     end
 end    
+legend('original','degree1','degree2','degree3')
